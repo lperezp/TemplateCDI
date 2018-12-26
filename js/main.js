4a6b234b87f2b3a4091bbdc3ca59820f0c109a57
Alberto Alcarraz Nuñez <alberto.alcarraz@cdiproject.com>
2018-12-26 15:15:26 -0500

    feat(registroUsuario)

--- js/main.js
+++ js/main.js
@@ -12,4 +12,11 @@ $(document).ready(function () {
         $("#sign-out").addClass("hover");
     })
 
+    $('#tabla_registro').tablesorter(); 
+    bsCustomFileInput.init()
+        var btn = document.getElementById('btnResetForm')
+        var form = document.querySelector('form')
+        btn.addEventListener('click', function () {
+          form.reset()
+        })
 });
