73e08fc7be3d1257eddb30ce7245fda0bac4cfce
Alberto Alcarraz Nuñez <alberto.alcarraz@cdiproject.com>
2018-12-27 17:55:12 -0500

    feat(registroHuella&registroHorario)

--- js/main.js
+++ js/main.js
@@ -2,21 +2,25 @@ $(document).ready(function () {
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
+    /* $("#dedo").toggleClass("hover"); */
     });
     
     $("#btn-sign-out").on("mouseover",function(){
-        $("#sign-out").removeClass("hover");
-    })
+        $("#sign-out").toggleClass("hover");
+    });
 
-    $("#btn-sign-out").on("mouseout",function(){
-        $("#sign-out").addClass("hover");
-    })
+    $('.card button').on('mouseover', function () {
+        $(".card #dedoi").toggleClass("hidden");
+     });
 
     $('#tabla_registro').tablesorter(); 
-    bsCustomFileInput.init()
-        var btn = document.getElementById('btnResetForm')
-        var form = document.querySelector('form')
+    bsCustomFileInput.init();
+        var btn = document.getElementById('btnResetForm');
+        var form = document.querySelector('form');
         btn.addEventListener('click', function () {
           form.reset()
-        })
+        });
+        
+    
+    
 });
